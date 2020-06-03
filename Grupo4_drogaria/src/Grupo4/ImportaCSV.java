2426081bf276d96379db411a544934208abfc4de
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2020-06-03 04:23:50 -0300

    Modifica a implementacao da classe LerArquivo aplica padrao singleton.

--- Grupo4_drogaria/src/Grupo4/ImportaCSV.java
+++ Grupo4_drogaria/src/Grupo4/ImportaCSV.java
@@ -1,28 +1,51 @@
 package Grupo4;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ImportaCSV {
 	
-	public void Ler(String caminho) {
-		File arquivoCSV = new File(caminho);
+	private ImportaCSV() {}
+
+	private static BufferedReader conteudoCsv = null;
+	
+	public static void lerArquivoCSV(String csvArquivo) {
+		
+		//String csvArquivo = ".\\Dados_DB\\MOCK_DATA.csv";
 		
 		try {
-			String linhasDoArquivo = new String();
-			
-			Scanner leitor = new Scanner(arquivoCSV);
-			
-			while(leitor.hasNext()) {
-				linhasDoArquivo = leitor.nextLine();
-				
-				String[] valoresEntreVirgulas = linhasDoArquivo.split(",");
-			
-				System.out.println(valoresEntreVirgulas[1]+" , "+valoresEntreVirgulas[4]+" , "+valoresEntreVirgulas[5]);
+			if(conteudoCsv == null) {
+				conteudoCsv = new BufferedReader(new FileReader(csvArquivo));
+				String linha ="";
+				while ((linha = conteudoCsv.readLine()) != null) {
+					String[] Produtos = linha.split(",");
+					
+					System.out.println("[id = " 	 + Produtos[0]+"]" 
+							+", [nome da marca = " + Produtos[1]+"]"
+							+", [nome generico = " + Produtos[2]+"]"
+							+", [laboratorio = "   + Produtos[3]+"]"
+							+", [quantidade = "    + Produtos[4]+"]"
+							+", [preco = "	     + Produtos[5]+"]"
+							);
+				}
 			}
-		}catch(FileNotFoundException e) {
-			System.out.println("errrro");
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo nao encontrado: \n"+e.getMessage());
+		}catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("indexOutOfBounds: \n"+e.getMessage());
+		}catch (IOException e) {
+			System.out.println("IO Erro: \n"+ e.getMessage());
+		} finally {
+			if(conteudoCsv != null) {
+				try {
+					conteudoCsv.close();
+				} catch (IOException e) {
+					System.out.println("IO Erro: \n"+ e.getMessage());
+				}
+			}
 		}
 	}
+	
 }
--- Grupo4_drogaria/src/Grupo4/Principal_grupo04.java
+++ Grupo4_drogaria/src/Grupo4/Principal_grupo04.java
@@ -7,12 +7,13 @@ public class Principal_grupo04 {
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
 		//Conexao_db.criarConexao();
 		
-		new GraficoProdutos();
+		//new GraficoProdutos();
 		
 		//Conexao_db.desconectar();
 		
-		//ImportaCSV ler = new ImportaCSV();
-		//ler.Ler(".\\Dados_DB\\MOCK_DATA.csv");
+		
+		ImportaCSV.lerArquivoCSV(".\\Dados_DB\\MOCK_DATA.csv");
+		
 		
 	}
 
